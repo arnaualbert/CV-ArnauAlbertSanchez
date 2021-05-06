5abccf54fe09adbfca077fe1e7143aeb38e5bd5a
Arnau <random@random.random>
2021-05-06 15:40:56 +0200

    V7

--- js/code.js
+++
@@ -1,8 +0,0 @@
-function language() {
-    var idioma = document.getElementById('language').value
-    if (idioma == 'ES') {
-        window.location.href = '../index.html'
-    } else {
-        window.location.href = './en/index.html'
-    }
-}
